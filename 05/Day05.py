eee18e42161404b61b6c31e1720bda7702d23f8c
Jim Atkinson <j_git@atkinsons.net>
2023-12-05 01:27:25 -0800

    Day 5: Switched part 2 to interval math and it became simple

--- 05/Day05.py
+++ 05/Day05.py
@@ -3,6 +3,22 @@
 <Problem description here>
 """
 import sys
+from collections import namedtuple
+
+# An interval represents the range [begin .. end)
+class Interval(namedtuple('Interval', ['begin', 'end'])):
+    def __bool__(self):
+        # Invalid intervals evaluate as False
+        return self.begin < self.end
+    
+    def __add__(self, offset):
+        return Interval(self[0] + offset, self[1] + offset)
+    
+    def SplitAgainst(self, other):
+        return (Interval(self.begin, min(self.end, other.begin)),
+                Interval(max(self.begin, other.begin), min(self.end, other.end)),
+                Interval(max(self.begin, other.end), self.end))
+
 
 class Day05:
     def __init__(self):
@@ -37,42 +53,116 @@ class Day05:
 
 
     def Part1(self):
-        mapNames = ("seed-to-soil soil-to-fertilizer fertilizer-to-water"
-                    " water-to-light light-to-temperature temperature-to-humidity"
-                    " humidity-to-location").split()
-
-        answer = 99999999999
-        mapName = ""
+        mapName = None
         for line in self.lines:
             if line.startswith('seeds:'):
-                self.seeds = [int(seed) for seed in line.split(':')[-1].split()]
+                seeds = [int(seed) for seed in line.split(':')[-1].split()]
+                continue
             elif line.endswith(' map:'):
                 mapName = line[:-5]
+                seedMap = []
             else:
                 parts = line.split()
-                if len(parts) != 3:
+                if len(parts) == 3:
+                    dst, src, length = [int(part) for part in parts]
+                    seedMap.append((src, dst, length))
                     continue
-                dst, src, length = [int(part) for part in parts]
-                m = self.maps.setdefault(mapName, {})
-                for i in range(length):
-                    m[src + i] = dst + i
+                if not mapName:
+                    continue
+                self.DoMapSeeds(mapName, seedMap, seeds)
+                seedMap = []
 
-        for seed in self.seeds:
-            for mapName in mapNames:
-                prev = seed
-                seed = self.maps[mapName].get(seed, seed)
-                print(f'{prev} -> {mapName} -> {seed}')
-            
-            answer = min(seed, answer)
+        if seedMap:
+            self.DoMapSeeds(mapName, seedMap, seeds)        
 
+
+        answer = min(seeds)
             
         return answer
     
+    def DoMapSeeds(self, mapName, seedMap, seeds):
+        "Modifies seeds in-place"
+        seedMap.sort()
+
+        for i, seed in enumerate(seeds):
+            prev = seed
+            for src, dst, length in seedMap:
+                if src > seed:
+                    break
+                if src <= seed < src + length:
+                    seeds[i] = seed - src + dst
+                    break
+            # print(f'{prev} -> {mapName} -> {seeds[i]}')
 
     def Part2(self):
-        answer = 0
+        seeds = [int(seed)
+                 for seed in self.lines[0].split(':')[-1].split()]
+        seedInts = [Interval(seeds[i], seeds[i] + seeds[i+1])
+                    for i in range(0, len(seeds), 2)]
+
+        mapName = None
+        for line in self.lines[2:]:
+            if line.endswith(' map:'):
+                mapName = line[:-5]
+                seedMap = []
+            else:
+                parts = line.split()
+                if len(parts) == 3:
+                    dst, src, length = [int(part) for part in parts]
+                    seedMap.append((Interval(src, src + length), dst - src))
+                    continue
+                if not mapName:
+                    continue
+                seedInts = self.DoMapSeedInts(mapName, seedMap, seedInts)
+                seedMap = []
+
+        if seedMap:
+            seedInts = self.DoMapSeedInts(mapName, seedMap, seedInts)        
+
+        answer = min(pair[0] for pair in seedInts)
+            
         return answer
-    
+
+    def DoMapSeedInts(self, mapName, seedMap, seedInts):
+        seedInts = sorted(seedInts, reverse=True)
+        seedMap = sorted(seedMap, reverse=True)
+
+        # print(f'{mapName}:')
+        # print(f'  Seeds:')
+        # for item in seedInts:
+        #     print(f'    {item}')
+        # print(f'  Mappings:')
+        # for srcInt, offset in seedMap:
+        #     print(f'    {srcInt}, {offset}')
+
+        result = []
+        count = 0
+        srcInt, offset = seedMap.pop()
+        seedInt = seedInts.pop()
+        try:
+            while True:
+                before, during, after = seedInt.SplitAgainst(srcInt)
+                if before:
+                    result.append(before)
+                if during:
+                    result.append(during + offset)
+                if after:
+                    srcInt, offset = seedMap.pop()
+                    seedInt = after
+                else:
+                    seedInt = seedInts.pop()
+
+        except IndexError:
+            if after:
+                result.append(after)
+            result.extend(reversed(seedInts))
+
+        # print(f'  Returns:')
+        # for item in result:
+        #     print(f'    {item})')
+
+        return result
+
 if __name__ == '__main__':
     problem = Day05()
     
