3e5b459e249de939648cf8a10b107d4e21678a8b
Jim Atkinson <j_git@atkinsons.net>
2023-12-10 22:50:50 -0800

    Day11 refactor to reduce code
    Reimplement Part2 with an optional expansion factor argument and call
    it Part1. Part2 now just calls Part1 with a different expansion factor.

--- 11/Day11.py
+++ 11/Day11.py
@@ -37,37 +37,8 @@ class Day11:
         self.height = len(self.lines)
 
 
-    def Part1(self):
+    def Part1(self, factor=2):
         answer = 0
-        sky = np.zeros((self.height, self.width), dtype=int)
-
-        id = 0
-        for row, line in enumerate(self.lines):
-            for col, ch in enumerate(line):
-                if ch != '.':
-                    id += 1
-                    sky[row,col] = id
-
-        for row in reversed(range(sky.shape[0])):
-            if not any(sky[row,:]):
-                sky = np.insert(sky, row, 0, axis=0)
-
-        for col in reversed(range(sky.shape[1])):
-            if not any(sky[:,col]):
-                sky = np.insert(sky, col, 0, axis=1)
-
-        positions = np.argwhere(sky)
-        for i in range(len(positions)):
-            for j in range(i+1, len(positions)):
-                dist = (abs(positions[i][0] - positions[j][0]) +
-                        abs(positions[i][1] - positions[j][1]))
-                answer += dist
-
-        return answer
-
-    def Part2(self):
-        answer = 0
-        factor = 1_000_000
         sky = np.zeros((self.height, self.width), dtype=int)
 
         id = 0
@@ -114,6 +85,9 @@ class Day11:
                 answer += dist
 
         return answer
+    
+    def Part2(self):
+        return self.Part1(factor=1_000_000)
     
 if __name__ == '__main__':
     problem = Day11()
