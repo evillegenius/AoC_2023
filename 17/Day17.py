2f53252a695cfb3e13bb4b12736dfc5a8d546946
Jim Atkinson <j_git@atkinsons.net>
2023-12-31 00:21:34 -0800

    Finished!
    Day 17 was giving me some problems so I went and watched a movie with my family.
    After the movie was over, I knew where the off-by-one error was.

--- 17/Day17.py
+++ 17/Day17.py
@@ -2,9 +2,11 @@
 """
 <Problem description here>
 """
-import sys
+import os
 import numpy as np
 from collections import namedtuple
+from dataclasses import dataclass
+import itertools
 import math
 
 NORTH = 0
@@ -39,18 +41,23 @@ class Day17:
 
 
     def ParseInput(self):
-        with open(self.input) as input:
-            self.lines = input.read().strip().split('\n')
+        if os.path.isfile(self.input + ".npy"):
+            self.cost = np.load(self.input + ".npy")
+            self.height, self.width = self.cost.shape
 
-        self.width = len(self.lines[0])
-        self.height = len(self.lines)
+        else:
+            with open(self.input) as input:
+                self.lines = input.read().strip().split('\n')
 
-        self.cost = np.zeros((self.height, self.width), dtype=int)
-        for row, line in enumerate(self.lines):
-            for col, ch in enumerate(line):
-                self.cost[row, col] = int(ch)
+            self.width = len(self.lines[0])
+            self.height = len(self.lines)
 
-        print(self.cost)
+            self.cost = np.zeros((self.height, self.width), dtype=int)
+            for row, line in enumerate(self.lines):
+                for col, ch in enumerate(line):
+                    self.cost[row, col] = int(ch)
+
+            np.save(self.input + ".npy", self.cost)
 
         self.huge = 10 ** (math.ceil(math.log10(self.cost.sum()))) - 1
 
@@ -133,14 +140,118 @@ class Day17:
         return answer
 
     def Part2(self):
-        answer = 0
+        # Ok, pseudo dynamic programming did not work that great. Try Dijkstra's
+        # algorithm for part 2.
+        #
+        # queue.PriorityQueue looks like it has too much baggage and does not
+        # allow you to adjust the priority of an item.
+        #
+        import heapq
+
+        class PriorityQueue:
+            """
+            Priority queue that lets you adjust the priority of a queued item.
+            Neither queue.PriorityQueue nor heapq allow you to do so directly.
+            Queue items must be hashable (i.e., immutable) and ordered.
+            """
+            def __init__(self):
+                self.heap = []
+                self.map = {}
+
+            def __len__(self):
+                return len(self.heap)
+
+            def Push(self, item, priority):
+                """
+                Push an item on the queue. If it was already there, adjust
+                its priority.
+                """
+                if item in self.map:
+                    self.map[item][0] = priority
+                    heapq.heapify(self.heap)
+                else:
+                    heapItem = [priority, item]
+                    self.map[item] = heapItem
+                    heapq.heappush(self.heap, heapItem)
+
+            def Pop(self):
+                priority, item = heapq.heappop(self.heap)
+                del self.map[item]
+                return (item, priority)
+
+        Item = namedtuple('Item', 'row col heading steps')
+
+        pending = PriorityQueue()
+
+        # Shape is designed so scores can be indexed by Item instances
+        shape = self.cost.shape + (4, 11)
+        scores = np.full(shape, self.huge, dtype=np.int32)
+
+        pending.Push(Item(0, 0, EAST, 0), 0)
+        pending.Push(Item(0, 0, SOUTH, 0), 0)
+
+        def GoStraight(item):
+            row, col, heading, steps = item
+            if   heading == NORTH: dr, dc = NORTH_DIR
+            elif heading == WEST:  dr, dc = WEST_DIR
+            elif heading == SOUTH: dr, dc = SOUTH_DIR
+            elif heading == EAST:  dr, dc = EAST_DIR
+            else:
+                assert False, f"Unknown heading: {heading!r}"
+
+            advance = max(1, 4 - steps)
+            # Make sure we won't step off the edge of the world
+            testRow = row + dr * advance
+            testCol = col + dc * advance
+            if 0 <= testRow < self.height and 0 <= testCol < self.width:
+                return Item(row + dr, col + dc, heading, steps + 1)
+            
+            return None
+
+        def TurnLeft(item):
+            return GoStraight(item._replace(heading=(item.heading + 1) % 4, steps=0))
+        
+        def TurnRight(item):
+            return GoStraight(item._replace(heading=(item.heading - 1) % 4, steps=0))
+
+        targetPos = (self.height - 1, self.width - 1)
+        while pending:
+            item, score = pending.Pop()
+            row, col, heading, steps = item
+
+            if (row, col) == targetPos and steps > 3:
+                answer = score
+                break
+
+            neighbors = []
+            if steps > 3:
+                # We can turn
+                if leftNeighbor := TurnLeft(item):
+                    neighbors.append(leftNeighbor)
+                if rightNeighbor := TurnRight(item):
+                    neighbors.append(rightNeighbor)
+            if steps < 10:
+                # We can go straight
+                if aheadNeighbor := GoStraight(item):
+                    neighbors.append(aheadNeighbor)
+
+            for neighbor in neighbors:
+                # An Item like neighbor starts with a row and column, so
+                # neighbor[:2] indexes nicely into self.cost.
+                newScore = score + self.cost[neighbor[:2]]
+                if newScore < scores[neighbor]:
+                    scores[neighbor] = newScore
+                    # Note: if neighbor is already queued, Push will just
+                    # reprioritize it with newScore
+                    pending.Push(neighbor, newScore)
+
         return answer
     
 if __name__ == '__main__':
     problem = Day17()
     
-    answer1 = problem.Part1()
-    print(f'Answer 1: {answer1}')
+    # answer1 = problem.Part1()
+    # print(f'Answer 1: {answer1}')
 
     answer2 = problem.Part2()
     print(f'Answer 2: {answer2}')
